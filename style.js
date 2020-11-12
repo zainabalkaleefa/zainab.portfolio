9aef4e3f2bec5f1141c34c8d49edb7c1e3d385e0
zainab.alkaleefa <zainab.alkaleefa@gmail.com>
2020-11-12 00:24:05 +0000

    add @media query

--- style.js
+++ style.js
@@ -1,5 +1,6 @@
 const hamburger =document.querySelector('.header .navBar .navList .hamburger');
 const mobileMenu =document.querySelector('.header .navBar .navList ul');
+const menuItem =document.querySelectorAll('.header .navBar .navList ul li a');
 const header = document.querySelector('.header .container');
 
 hamburger.addEventListener('click',()=>{
@@ -7,3 +8,9 @@ hamburger.addEventListener('click',()=>{
     mobileMenu.classList.toggle('active');
 });
 
+menuItem.forEach(item=>{
+  item.addEventListener('click',()=>{
+    hamburger.classList.toggle('active');
+    mobileMenu.classList.toggle('active');  
+  })  
+})
